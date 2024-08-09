be59d3b7d401370d63d4c208e44d415a6ca0d20c
Raminta <1989sliekiukasss@gmail.com>
2024-08-09 16:13:23 +0300

    buttons update

--- src/components/tasks-list/Task.jsx
+++ src/components/tasks-list/Task.jsx
@@ -1,48 +1,86 @@
 import { useState } from 'react';
 
-export function Task() {
-    const [count, setCount] = useState(0);
+export function Task(params) {
+    const data = params.data;
+    const text = data.text;
 
-    function updateCount1() {
-        setCount(count + 1);
-    }
-    function updateCount2() {
-        setCount(count + 2);
-    }
-    function updateCount3() {
-        setCount(count + 3);
-    }
+    // arba galem pasirasyti sitaip:
+    // const {text} = params.data;
+
+    const [editForm, setEditForm] = useState(false);
+    const [taskText, setTaskText] = useState(text);
+    const [inputText, setInputText] = useState(text);
+
+    // hooks'ai
+    // const [count, setCount] = useState(0);
+
+    // function updateCount1() {
+    //     setCount(count + 1);
 
     const style = {
         borderLeftColor: '#ff0000',
     };
+
+    function handleUpdate(e) {
+        e.preventDefault();
+
+        const cleanText = inputText.trim();
+        if (cleanText !== '') {
+            setTaskText(cleanText);
+            setInputText(cleanText);
+            setEditForm(false);
+        }
+    }
 
     return (
         <div className="list">
             <article className="item" data-state="" style={style}>
                 <div className="date">2024-08-08 11:38:20</div>
                 <div className="state">Atlikta</div>
-                <div className="text">What should i do({count})</div>
-                <form className="hidden">
-                    <input type="text" value="What should i do" />
-                    <button className="update" type="submit">
-                        Update
-                    </button>
-                    <button className="cancel" type="button">
-                        Cancel
-                    </button>
+                <div className="text">{taskText}</div>
+                <form
+                    onSubmit={handleUpdate}
+                    className={editForm ? '' : 'hidden'}
+                >
+                    <input
+                        onChange={(e) => setInputText(e.target.value)}
+                        type="text"
+                        value={inputText}
+                    />
+                    <div className="btnList">
+                        <button
+                            onClick={() => setInputText(taskText)}
+                            className="clear"
+                            type="reset"
+                        >
+                            Reset
+                        </button>
+                        <button
+                            onClick={() => setInputText('')}
+                            className="clear"
+                            type="reset"
+                        >
+                            Clear
+                        </button>
+                        <button className="update" type="submit">
+                            Update
+                        </button>
+                        <button
+                            onClick={() => setEditForm(false)}
+                            className="cancel"
+                            type="button"
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </form>
                 <div className="actions">
-                    <button className="done" onClick={updateCount1}>
-                        Done
-                    </button>
+                    <button className="done">Done</button>
                     <div className="divider"></div>
-                    <button className="edit" onClick={updateCount2}>
+                    <button className="edit" onClick={() => setEditForm(true)}>
                         Edit
                     </button>
-                    <button className="delete" onClick={updateCount3}>
-                        Delete
-                    </button>
+                    <button className="delete">Delete</button>
                 </div>
             </article>
         </div>
--- src/components/tasks-list/TaskList.jsx
+++ src/components/tasks-list/TaskList.jsx
@@ -1,7 +1,20 @@
 import { Task } from './Task';
 
 export function TaskList() {
-    const data = [1, 1, 1, 1, 1];
+    const data = [
+        {
+            text: 'Issivalyti dantis',
+        },
+        {
+            text: 'Nusivalyti makiaza',
+        },
+        {
+            text: 'Pasigaminti vakariene',
+        },
+        {
+            text: 'Sesti prie PC',
+        },
+    ];
 
     if (data.length === 0) {
         return <div className="list empty">Empty</div>;
@@ -9,7 +22,7 @@ export function TaskList() {
     return (
         <div className="list">
             {data.map((item, index) => (
-                <Task key={index} />
+                <Task key={index} data={item} />
             ))}
         </div>
     );
