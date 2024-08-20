310e4b379299163b36edb2310271a3ec73612481
Raminta <1989sliekiukasss@gmail.com>
2024-08-20 15:44:29 +0300

    data tvarkausi

--- src/components/tasks-list/TaskList.jsx
+++ src/components/tasks-list/TaskList.jsx
@@ -1,15 +1,20 @@
 import { Task } from './Task';
 
 export function TaskList(params) {
-    const { updateTaskText, updateTaskColor, updateTaskState, removeTask } =
-        params;
+    const {
+        data,
+        updateTaskText,
+        updateTaskColor,
+        updateTaskState,
+        removeTask,
+    } = params;
 
-    if (params.data.length === 0) {
+    if (data.length === 0) {
         return <div className="list empty">Empty</div>;
     }
     return (
         <div className="list">
-            {params.data.map((item) => (
+            {data.map((item) => (
                 <Task
                     key={item.id}
                     data={item}
